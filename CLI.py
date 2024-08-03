25099facae93bfa20711a9407f7ef33f8b5d8c53
11-pog <alvessamuel872@gmail.com>
2024-08-03 18:07:22 -0300

    why tf did readme disappear

--- CLI.py
+++ CLI.py
@@ -5,11 +5,12 @@ from MCModDownloader import MCModDownloader
 if __name__ == "__main__":
     MCMD = MCModDownloader()
 
-    parser = argparse.ArgumentParser(description="Mod Downloader")
+    parser = argparse.ArgumentParser(description="Download minecraft mods from Modrinth and Curseforge automatically (peak laziness)")
 
     input = parser.add_mutually_exclusive_group(required=True)
-    input.add_argument("--ml", "--mod-list", help="List of mods to download, based on a .txt file with the links", metavar="MODS.TXT")
     input.add_argument("-m", "--mod-link", help="Single mod download, use a link", metavar="MOD LINK")
+    input.add_argument("--ml", "--mod-list", help="Download a bunch of mods simultaneously", metavar="MOD LINKS", nargs="+")
+    input.add_argument("--mltxt", "--mod-list-txt", help="Download the mods from a txt file containing one mod link per line", metavar="TXT FILE")
 
     parser.add_argument("-g", "--game-version", help="Version of minecraft for the mod (eg: 1.19.2, 1.20.1, etc)", nargs='+')
     parser.add_argument("-l", "--loader", help="The mod loader for this mod (eg: forge, neoforge, fabric)", default=["forge"], nargs='+')
@@ -28,5 +29,26 @@ if __name__ == "__main__":
     if args.mod_link is not None:
         name, mod = asyncio.run(MCMD.download_latest(args.mod_link, parameters))
         asyncio.run(MCMD.saveFile(mod, name, args.output))
+
+    elif args.ml is not None:
+        for link in args.ml:
+            name, mod = asyncio.run(MCMD.download_latest(link, parameters))
+            asyncio.run(MCMD.saveFile(mod, name, args.output))
+
+    else:
+        try:
+            with open(args.mltxt, 'r') as file:
+                for line in file:
+                    link = line.strip()
+
+                    if link:
+                        name, mod = asyncio.run(MCMD.download_latest(link, parameters))
+                        asyncio.run(MCMD.saveFile(mod, name, args.output))
+                    
+        except FileNotFoundError:
+            print(f"ERROR: Input file {args.mltxt} was not found")
+        except Exception as e:
+            print(f"An error occurred: {e}")
+
 
     #Mod list is WIP
--- MCSiteAPI.py
+++ MCSiteAPI.py
@@ -1,15 +1,18 @@
 import aiohttp
+import asyncio
+import os
+from furl import furl
 
-async def _fetch_data(url):
+async def _get(url, *, headers = None, params = None):
     async with aiohttp.ClientSession() as session:
-        async with session.get(url) as response:
+        async with session.get(url, headers=headers, params=params) as response:
             if response.status == 200:
                 if 'application/json' in response.headers.get('Content-Type', ''):
                     return await response.json()
                 else:
                     return await response.text()
             else:
-                return None
+                raise Exception(f"Http get error {response.status}: {response.reason}")
             
 async def _Dl_Data(url):
     async with aiohttp.ClientSession() as session:
@@ -17,13 +20,13 @@ async def _Dl_Data(url):
             if response.status == 200:
                 return await response.read()
             else:
-                return None
+                raise Exception(f"Http get error {response.status}: {response.reason}")
             
 def _formatParams(params):
     return ",".join(f'"{value}"' for value in params)
 
-def _splitUrl(url):
-        splitUrl = url.split('/mod/')
+def _splitUrl(url, separator):
+        splitUrl = url.split(separator)
 
         if len(splitUrl) == 2:
             return splitUrl[1]
@@ -34,17 +37,23 @@ class ModrinthAPI:
     def __init__(self, api_url="https://api.modrinth.com"):
         self.api_url = api_url
 
+
     
     async def get_project(self, url):
-        response = await _fetch_data(f"{self.api_url}/v2/project/{_splitUrl(url=url)}")
+        response = await _get(f"{self.api_url}/v2/project/{_splitUrl(url, '/mod/')}")
         return response
 
+
+
     async def get_version(self, version_id):
-        response = await _fetch_data(f"{self.api_url}/v2/version/{version_id}")
+        response = await _get(f"{self.api_url}/v2/version/{version_id}")
         return response
     
-    async def project_versions(self, project_slug, parameters):
+
+
+    async def project_files(self, project_slug, parameters):
         query = f"{self.api_url}/v2/project/{project_slug}/version"
+        
         if parameters:
             version = parameters['game_versions']
             loader = parameters['loader']
@@ -62,10 +71,12 @@ class ModrinthAPI:
             if queryParams:
                 query += "?" + "&".join(queryParams)
 
-        fetchResult = await _fetch_data(query)
+        fetchResult = await _get(query)
         response = sorted(fetchResult, key=lambda x: x['date_published'], reverse=True)
         return response
     
+
+
     async def download(self, parameters = None,  *, version_id = None, url = None):
         version = None
         if not (version_id or (parameters and url)):
@@ -74,8 +85,8 @@ class ModrinthAPI:
         if version_id:
             version = await ModrinthAPI.get_version(self, version_id)
         else:
-            project_slug = _splitUrl(url=url)
-            versionList = await ModrinthAPI.project_versions(self, project_slug, parameters)
+            project_slug = _splitUrl(url, '/mod/')
+            versionList = await ModrinthAPI.project_files(self, project_slug, parameters)
             version = versionList[0]
 
         fileUrl = version['files'][0]['url']
@@ -83,8 +94,48 @@ class ModrinthAPI:
         return version, response
 
     
+
 class CurseforgeAPI:
     def __init__(self, api_url="https://api.curseforge.com"):
         self.api_url = api_url
+        api_key = os.environ.get('CF_API_KEY')
 
-    #work in progress
+        if api_key is None:
+            raise ValueError("CF_API_KEY enviroment variable is not set")
+        
+        self.api_headers = {
+        'Accept': 'application/json',
+        'x-api-key': api_key
+        }
+
+    async def get_id_by_slug(self, url):
+        slug = _splitUrl(url, '/mc-mods/')
+        data = await _get(f'{self.api_url}/v1/mods/search', headers=self.api_headers, params={
+            'slug': slug,
+            'classId': '6',
+            'gameId': '432'
+        })
+
+        result = data['data'][0]['id']
+
+        return result
+    
+    async def project_files(self, url, parameters=None):
+
+        if parameters:
+            CFParams = {
+
+
+                
+            }
+
+
+        id = await self.get_id_by_slug(url)
+        files = await _get(f'{self.api_url}/v1/mods/{id}/files', headers=self.api_headers)
+
+        result = sorted(files['data'], key=lambda x: x['fileDate'], reverse=True)
+
+        print(result)
+       
+CFAPI = CurseforgeAPI()
+print(asyncio.run(CFAPI.project_files('https://www.curseforge.com/minecraft/mc-mods/mouse-tweaks')))
