55056ddc95d440989f2884793d3be7ec7817a7b6
11-pog <alvessamuel872@gmail.com>
2024-08-02 22:38:48 -0300

    Nothing Too Important

--- MCModDownloader.py
+++ MCModDownloader.py
@@ -3,12 +3,18 @@ import aiohttp
 async def fetch_data(url):
     async with aiohttp.ClientSession() as session:
         async with session.get(url) as response:
-            return await response.json()
+            if response.status == 200:
+                return await response.json()
+            else:
+                return None
             
 async def Dl_Data(url):
     async with aiohttp.ClientSession() as session:
         async with session.get(url, headers = {"Accept": "application/octet-stream"}) as response:
-            return await response.read()
+            if response.status == 200:
+                return await response.read()
+            else:
+                return None
 
 class ModrinthAPI:
     def __init__(self, api_url="https://api.modrinth.com"):
@@ -16,6 +22,10 @@ class ModrinthAPI:
 
     async def get_project(self, project_id):
         response = await ModrinthAPI.fetch_data(f"{self.api_url}/v2/project/{project_id}")
+        return response
+
+    async def get_project_by_link(self, url):
+        response = await ModrinthAPI.fetch_data(url)
         return response
 
     async def get_version(self, version_id):
