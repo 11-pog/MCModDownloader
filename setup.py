acbc9792c8c062daf799a78e11392fb5fe109312
11-pog <alvessamuel872@gmail.com>
2024-08-07 09:55:14 -0300

    package version is not 1.0.dev1 and dist is now available

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
     
 setuptools.setup(
     name="mcmm",
-    version="0.1.2",
+    version="1.0.dev1",
     author="OpinionThief",
     author_email="alvessamuel872@gmail.com",
     description="A package to download Minecraft mods from Modrinth and Curseforge, comes with free spaghetti (code)",
